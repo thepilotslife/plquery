57b7e7bb2a3a8d0dc3462d0cd8da2f93d514a917
yugecin <yugecin@users.noreply.github.com>
2018-06-05 21:34:00 +0200

    pad last24players graph with zeros

--- last24players.php
+++ last24players.php
@@ -1,5 +1,7 @@
 <?php
 
+set_time_limit(3);
+
 require('db.php');
 
 $s = $db->prepare('select t.t, count(t.i) as players from t where t > unix_timestamp() - 86400 group by t.t order by t.t asc');
@@ -10,9 +12,13 @@ $values = array();
 
 $errcount = 0;
 $prevplayers = 0;
+$min = 0;
 $prev = 0;
 // if the first count is 0, then the start time has no data
 foreach ($d as $p) {
+	if ($min == 0) {
+		$min = $p->t;
+	}
 	if ($prev != 0) {
 		$dif = round(($p->t - $prev) / 300);
 		for ($i = 0; $i < $dif; $i++) {
@@ -27,6 +33,32 @@ foreach ($d as $p) {
 	$values[date('H:i', $p->t)] = $p->players;
 	$prevplayers = $p->players;
 	$prev = $p->t;
+}
+
+if (count($values) == 0) {
+	$_t = time();
+	$errcount = 288;
+	for ($i = 288; $i > 0; $i--) {
+		$values[date('H:i', $_t)] = 0;
+		$_t -= 300;
+	}
+}
+
+$_t = $prev;
+while (count($values) < 288) {
+	$t += 300;
+	if ($t > time()) {
+		break;
+	}
+	$values[date('H:i', $_t)] = 0;
+	$errcount++;
+}
+
+$_t = $min;
+while (count($values) < 288) {
+	$t -= 300;
+	$values[date('H:i', $_t)] = 0;
+	$errcount++;
 }
 
 require_once('SVGGraph/SVGGraph.php');
@@ -49,6 +81,10 @@ $settings = array(
   'axis_text_angle_h' => -90,
   'grid_division_h' => 6,
 );
+
+if ($errcount == 288) {
+  $settings['axis_max_v'] = 1;
+}
  
 $colours = array('rgba(255,255,0,0.5)');
 //$colours = array(array('blue', 'white'));
